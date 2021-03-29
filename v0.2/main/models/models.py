6f9024dcac243ba62d59cc0bcd110f8f862a4798
Mike Bionic <muhammedjepbarov@gmail.com>
2021-03-30 00:06:04 +0500

    updates in models - foreign key joins

--- v0.2/main/models/models.py
+++ v0.2/main/models/models.py
@@ -1,15 +1,22 @@
 from main import db
+class ResCategory(db.Model):
+	ResCatId = db.Column("ResCatId",db.Integer, primary_key=True)
+	ResCatName = db.Column("ResCatName",db.String(100))
+	Resource = db.relationship("Resources", backref="rescategory", lazy=True)
+	
+
 
 class Resource(db.Model):
 	__tablename__ = 'tbl_dk_resource'
 	ResId = db.Column("ResId",db.Integer, primary_key=True)
-	ResCatId = db.Column("ResCatId",db.Integer)
+	ResCatId = db.Column("ResCatId", db.ForeignKey("ResCategory.ResCatId"))
 	BrandId = db.Column("BrandId",db.Integer)
 	UsageStatusId = db.Column("UsageStatusId",db.Integer)
 	ResRegNo = db.Column("ResRegNo", db.String)
 	ResName = db.Column("ResName", db.String)
 	ResDesc = db.Column("ResDesc", db.String)
 	ResFullDesc = db.Column("ResFullDesc", db.String)
+
 
 	def to_json(self):
 		data = {
@@ -29,3 +36,5 @@ class Users(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	username = db.Column(db.String)
 	age = db.Column(db.Integer)
+
+
